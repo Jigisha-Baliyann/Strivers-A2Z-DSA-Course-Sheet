68f7769d29a80ab1c1f38de6db1d39d666d584f3
Jigisha Baliyann <115288853+Jigisha-Baliyann@users.noreply.github.com>
2023-11-19 18:38:37 +0530

    Update and rename For Loops to ForLoops.java

--- Step 01 Learn the Basics/Step 1.1 Things to know in Java/ForLoops.java
+++ Step 01 Learn the Basics/Step 1.1 Things to know in Java/ForLoops.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Solution {
+public class ForLoops {
 	public static void main(String[] args) {
 		/* Your class should be named Solution.
 	 	* Read input as specified in the question.
